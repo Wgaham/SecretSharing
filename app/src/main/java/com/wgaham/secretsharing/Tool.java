c7500f9a1ec8aa43195497aeea181f1ac73a4071
wgaham <jszzhaihao@live.com>
2018-03-19 20:41:39 +0800

    3/18 CRLF->LF

--- app/src/main/java/com/wgaham/secretsharing/Tool.java
+++ app/src/main/java/com/wgaham/secretsharing/Tool.java
@@ -1,9 +1,30 @@
 package com.wgaham.secretsharing;
 
+import java.util.Calendar;
+
 /**
- * Created by zh on 2018/3/13.
+ * 一些判断函数
+ *
+ * @author Wgaham
+ *         Created by zh on 2018/3/13.
  */
 
 public class Tool {
 
+    public static boolean rebirthTimeJudgment(String startTime, String endTime) {
+        /**
+         * 夹击妹抖略略略
+         */
+        int startHour, endHour, startMinute, endMinute, hour, minute;
+        if (startTime.equals(endTime)) {
+            return true;
+        }
+        Calendar calendar = Calendar.getInstance();
+        if (calendar.get(Calendar.AM_PM) == 0) {
+            hour = calendar.get(Calendar.HOUR);
+        } else {
+            hour = calendar.get(Calendar.HOUR) + 12;
+        }
+        minute = calendar.get(Calendar.MINUTE);
+    }
 }
